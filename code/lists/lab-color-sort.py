4442b7ddfb9d34a5b5dd93789d747ab76abc9f6f
Elise Deitrick <edeitrick@codio.com>
2020-01-31 22:41:54 +0000

    initial commit

--- code/lists/lab-color-sort.py
+++
@@ -1,4 +0,0 @@
-warm = ["red", "orange", "yellow", "burgundy", "pink", "rose", "pumpkin", "marigold", "gold", "citron", "amber"]
-cool = ["blue", "green", "purple", "cyan", "violet", "indigo", "azure", "teal", "mint", "lime", "emerald"]
-neutral = ["black", "white", "gray", "grey", "brown", "beige"]
-
--- code/selection/timer.py
+++
@@ -1,6 +0,0 @@
-import lab4 as program
-import time
-
-start_time = time.time()
-program
-print("--- %s seconds ---" % (time.time() - start_time))
